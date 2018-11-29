86c1d869a24bb0abb940baf0cf1b5dcccdc5d6ae
dataman17 <43328387+dataman17@users.noreply.github.com>
2018-11-29 08:54:13 -0800

    Add files via upload

--- src/Abdulla/main.cpp
+++ src/Abdulla/main.cpp
@@ -3,10 +3,12 @@
 #include <QApplication>
 #include <QException>
 #include <QtDebug>
+#include <time.h>
 
 class WidgetVP;
 
 int main(int argc, char *argv[]){
+    srand(time(NULL));
     QApplication a(argc, argv);
     MainWindow w;
 
