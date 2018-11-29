38adf5dddedacf5b464412493e3393fa1a3d99ae
dataman17 <43328387+dataman17@users.noreply.github.com>
2018-11-29 06:40:41 -0800

    Add files via upload

--- inc/DP_gametesting.h
+++ inc/DP_gametesting.h
@@ -13,18 +13,24 @@ class GameTesting : public QWidget
 {
     Q_OBJECT
 public:
-
+    //sets the attribute aMainWindow
     void SetAMainWindow(MainWindow * mw);
+    //Gets the singleton instance of GameTesting
     static GameTesting * GetInstance();
 
 signals:
 
 public slots:
+    //Plays the win demo mode
     void OnWinScriptSelected();
+    //plays the loose demo mode
     void OnLooseScriptSelected();
 private:
+    //instance of mainwindow that is used to create pop up widgets
     MainWindow * aMainWindow;
+    //unique singleton instance of GameTesting
     static GameTesting * aUniqueInstance;
+    //GameTesting constructor
     explicit GameTesting(QWidget *parent = nullptr);
 };
 
