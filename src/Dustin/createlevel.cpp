2b125308da96de03f603631c02eeff2eefd950c5
dataman17 <43328387+dataman17@users.noreply.github.com>
2018-11-29 11:34:55 -0800

    Add files via upload

--- src/Dustin/createlevel.cpp
+++ src/Dustin/createlevel.cpp
@@ -19,31 +19,33 @@ void CreateLevel::SetAMainWindow(MainWindow * mw){
 //generates a new level
 void CreateLevel::GenerateLevel(){
     int i = 0, j;
-    QString filename = ":Randomly_Generated_Level.txt";
+    QString errmsg;
+    QFileDevice::FileError err = QFileDevice::NoError;
+    //find the current directory
+
+    QString directory = QDir::currentPath();
+    QDir::setCurrent("..");
+    QString filename = QDir::currentPath();
+    QDir::setCurrent(directory);
+    filename.append("/Randomly_Generated_File.txt");
+    qDebug() << filename << endl;
     QFile file(filename);
-    if(!file.exists()){
-        qDebug() << "Error, File does not exist" << endl;
-        QMessageBox *Msgbox = new QMessageBox(aMainWindow);
-        Msgbox->setIcon(QMessageBox::Critical);
-        Msgbox->setGeometry(300, 400, 200, 50);
-        Msgbox->setText("Error: Something went wrong !");
-        Msgbox->exec();
-        delete Msgbox;
-        }
-    else{
+
         //if the file is successfully opened for write only
         if(file.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text)){
             qDebug() << "opened successfully" << endl;
             QTextStream stream(&file);
+            qDebug() <<"writing to file" << endl;
             //highest level of the level
+
             stream << "f ";
-            for(i = 0; i < 75; i++){
+            for(i = 0; i < 300; i++){
                 stream << "0";
             }
             stream << endl;
             //next level down (some chance of an enemy)
             stream << "e ";
-            for(i = 0; i < 75; i++){
+            for(i = 0; i < 300; i++){
                 j = rand() % 30;
                 //1/30 to create a critter
                 if(j == 0){
@@ -60,7 +62,7 @@ void CreateLevel::GenerateLevel(){
             stream << endl;
             //next level, row d
             stream << "d ";
-            for(i = 0; i < 75; i++){
+            for(i = 0; i < 300; i++){
                 j = rand() % 30;
                 //2/30 to create a critter
                 if(j < 2){
@@ -70,6 +72,12 @@ void CreateLevel::GenerateLevel(){
                 else if(j == 2 || j == 3){
                     stream << "6";
                 }
+                else if(j >3 && j < 6){
+                    stream << "7";
+                }
+                else if(j > 6 && j< 10){
+                    stream << "9";
+                }
                 else{
                     stream << "0";
                 }
@@ -78,7 +86,7 @@ void CreateLevel::GenerateLevel(){
             file.flush();
             //next level, row c
             stream << "c ";
-            for(i = 0; i < 75; i++){
+            for(i = 0; i < 300; i++){
                 j = rand() % 30;
                 //4/30 to create a critter
                 if(j < 4){
@@ -92,6 +100,9 @@ void CreateLevel::GenerateLevel(){
                 else if(j > 9 && j < 15){
                     stream << "7";
                 }
+                else if(j > 15 && j< 25){
+                    stream << "9";
+                }
                 else{
                     stream << "0";
                 }
@@ -99,41 +110,41 @@ void CreateLevel::GenerateLevel(){
             stream << endl;
             //next level, row b
             stream << "b ";
-            for(i = 0; i < 75; i++){
-                j = rand() % 30;
-                //4/30 to create a critter
-                if(j < 4){
-                    stream << "4";
-                }
-                //4/30 to create a swordsman
-                else if(j > 4 && j < 9){
-                    stream << "6";
-                }
-                //10/30 to create a block
-                else if(j > 9 && j < 20){
-                    stream << "1";
-                }
-                else{
-                    stream << "0";
-                }
+            for(i = 0; i < 300; i++){
+                stream << "7";
             }
             stream << endl;
             //lowest level of the level
             stream << "a ";
-            for(i = 0; i < 75; i++){
+            for(i = 0; i < 300; i++){
                 stream << "1";
             }
             stream << endl;
             file.close();
+            //print success message
+            QMessageBox *Msgbox = new QMessageBox(aMainWindow);
+            Msgbox->setIcon(QMessageBox::Critical);
+            Msgbox->setGeometry(300, 400, 200, 50);
+            Msgbox->setText("Success ! New level generated ! To play select Game->Level Selector->Randomly Generated !");
+            Msgbox->exec();
+            delete Msgbox;
+            aComplete = true;
         }
-        //print success message
-        QMessageBox *Msgbox = new QMessageBox(aMainWindow);
-        Msgbox->setIcon(QMessageBox::Critical);
-        Msgbox->setGeometry(300, 400, 200, 50);
-        Msgbox->setText("Success ! New level generated ! To play select Game->Level Selector->Randomly Generated !");
-        Msgbox->exec();
-        delete Msgbox;
-        aComplete = true;
-    }
-    return;
+        //print file error message
+        else{
+            errmsg = file.errorString();
+            err= file.error();
+            qDebug() << errmsg << endl;
+            qDebug() << err << endl;
+            //print success message
+            QMessageBox *Msgbox = new QMessageBox(aMainWindow);
+            Msgbox->setIcon(QMessageBox::Critical);
+            Msgbox->setGeometry(300, 400, 200, 50);
+            Msgbox->setText("Error ! The file could not be opened !");
+            Msgbox->exec();
+            delete Msgbox;
+            aComplete = true;
+        }
+
+        return;
 }
